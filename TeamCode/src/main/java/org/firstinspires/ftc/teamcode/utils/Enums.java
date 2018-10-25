a34002e499a1ca8a286faf84faf7b8e4349cca94
Meech <alexhwicklund@gmail.com>
2018-10-25 18:56:12 -0500

    Added Autonomous States

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Enums.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.utils;
+
+
+public class Enums {
+    public enum AutonomousStates {
+        LATCHED,
+        DROPPING,
+        DROPPED,
+        LOCATE_MINERAL,
+        DRIVE_TO_MINERAL,
+        AT_MINERAL,
+        PUSH_MINERAL,
+        DRIVE_TO_DEPOT,
+        DROP_MARKER,
+        STRAIGHTEN_ON_WALL,
+        DRIVE_TO_CRATER,
+        PARK_IN_CRATER,
+    }
+}
