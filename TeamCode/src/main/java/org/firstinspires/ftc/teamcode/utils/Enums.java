bbe917309c2371814e0a276563936ee91fac7c85
Meech <alexhwicklund@gmail.com>
2018-10-25 19:15:34 -0500

    Added code to run states

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Enums.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Enums.java
@@ -4,16 +4,15 @@ package org.firstinspires.ftc.teamcode.utils;
 public class Enums {
     public enum AutonomousStates {
         LATCHED,
-        DROPPING,
         DROPPED,
-        LOCATE_MINERAL,
-        DRIVE_TO_MINERAL,
+        MINERAL_LOCATED,
         AT_MINERAL,
-        PUSH_MINERAL,
-        DRIVE_TO_DEPOT,
-        DROP_MARKER,
-        STRAIGHTEN_ON_WALL,
-        DRIVE_TO_CRATER,
-        PARK_IN_CRATER,
+        MINERAL_PUSHED,
+        AT_DEPOT,
+        DROPPED_MARKER,
+        AT_WALL,
+        STRAIGHTENED_ON_WALL,
+        AT_CRATER,
+        PARKED_IN_CRATER,
     }
 }
